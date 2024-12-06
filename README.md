d05432f51ca6a844f00a58adbc27d32ebd285b56
czheo <1692400+czheo@users.noreply.github.com>
2024-12-05 22:35:16 -0800

    add start.sh

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+# Get started
+
+Prepare: get session from cookie in browser
+```bash
+export AOC_SESSION=...
+```
+
+Run below to start a day
+```bash
+./start.sh 2024 7
+```
+
 # Past years
 
 - [C++ 2023](https://github.com/czheo/aoc2023)
---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <year> <day>"
+    exit 1
+fi
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$1/day"$(printf "%02d" "$2")
+
+mkdir -p $DIR
+curl -H "Cookie: session=$AOC_SESSION" "https://adventofcode.com/$1/day/$2/input" -o $DIR/input.txt
+
+echo "cd $DIR"
