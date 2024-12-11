3500d6ad0a7b0f21313fdc9857ccbc7d32cac712
czheo <1692400+czheo@users.noreply.github.com>
2024-12-10 22:30:12 -0800

    refactor 2024 day 9 p2

--- 2024/day09/p2.py
+++ 2024/day09/p2.py
@@ -5,52 +5,44 @@ blocks = []
 line = sys.stdin.read().strip()
 
 free = False
-files = []
+blocks = {}
+frees = []
+i = 0
+fid = 0
 for c in line:
     n = int(c)
-    if free:
-        for _ in range(n):
-            blocks.append(-1)
-    else:
-        for _ in range(n):
-            blocks.append(len(files))
-        files.append((len(blocks) - n, len(blocks) - 1))
+    if n > 0:
+        j = i + n - 1
+        if free:
+            frees.append((i, j))
+        else:
+            blocks[fid] = (i, j)
+        i += n
     free = not free
+    if free:
+        fid += 1
 
-# print(blocks)
-# print(files)
+print(blocks)
+print(frees)
 
+for fid in reversed(range(fid)):
+    (bs, be) = blocks[fid]
+    for i, (fs, fe) in enumerate(frees):
+        if fe > bs:
+            break
+        if fe - fs >= be - bs:
+            blocks[fid] = (fs, fs + be - bs)
+            remain_free = fe - fs - (be - bs)
+            if remain_free > 0:
+                frees[i] = (fs + (be - bs) + 1, fe)
+            else:
+                frees.pop(i)
+            break
 
-def find_free(size, until):
-    global find_start
-    found_free = False
-    for i in range(find_start, until + 1):
-        if blocks[i] == -1:
-            found_free = True
-        if not found_free:
-            find_start = i
-        if sum(blocks[i : i + size]) == -1 * size:
-            return i
-    return -1
+ans = 0
+for fid, (s, e) in blocks.items():
+    ans += sum(fid * i for i in range(s, e + 1))
 
-
-def set_n(i, j, n):
-    while i <= j:
-        blocks[i] = n
-        i += 1
-
-
-find_start = 0
-fp = len(files) - 1
-while fp > 0:
-    # print(fp)
-    s, e = files[fp]
-    size = e - s + 1
-    idx = find_free(size, s - size)
-    if idx != -1:
-        set_n(s, e, -1)
-        set_n(idx, idx + size - 1, fp)
-    fp -= 1
-
-# print(blocks)
-print(sum(i * n for i, n in enumerate(blocks) if n >= 0))
+print(blocks)
+print(frees)
+print(ans)
