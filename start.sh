151e152cb78d155065a6b2c9d1ea8c10c748cafc
czheo <1692400+czheo@users.noreply.github.com>
2024-12-15 04:16:50 -0800

    update start.sh

--- start.sh
+++ start.sh
@@ -6,8 +6,10 @@ if [ "$#" -ne 2 ]; then
 fi
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$1/day"$(printf "%02d" "$2")
+URL="https://adventofcode.com/$1/day/$2"
 
 mkdir -p $DIR
-curl -H "Cookie: session=$AOC_SESSION" "https://adventofcode.com/$1/day/$2/input" -o $DIR/input.txt
+curl -H "Cookie: session=$AOC_SESSION" $URL"/input" -o $DIR/input.txt
 
 echo "cd $DIR"
+echo $URL
